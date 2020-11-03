4435d50779b4c3d066fe081ff5c981d4f2e908bf
Deepak Nagarkoti <45748958+Nagarkotideepak@users.noreply.github.com>
2020-11-03 07:26:26 +0545

    update in intro of java

---
+++ 00Introduction/Bin2Dec.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+class Bin2Dec
+{
+public static void main(String argos[])
+{
+    Scanner s = new Scanner(System.in);
+    s.useRadix(2);
+    int x=s.nextInt();
+	System.out.println(x);
+}
+}
