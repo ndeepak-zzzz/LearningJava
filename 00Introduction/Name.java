c19f267c7424c87e39579ebb3e86e22da3e07ba9
Deepak Nagarkoti <45748958+Nagarkotideepak@users.noreply.github.com>
2020-11-03 08:58:28 +0545

    update

--- 00Introduction/Name.java
+++ 00Introduction/Name.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
-class Name{
-    public static void main(String args[])
-    {
+class Name {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String name;
         System.out.println("What is your name? ");
         name = sc.nextLine();
-        System.out.println("Hello there, Mr/Miss "+ name);
+        System.out.println("Hello there, Mr/Miss " + name);
         sc.close();
     }
 }
